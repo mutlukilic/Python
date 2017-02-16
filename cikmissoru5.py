24526aab1984294cc97b4a457ce8d9a7461ce6f0
Mutlu Kılıç <mutlukilic1919@gmail.com>
2017-02-16 14:43:15 +0300

    Add files via upload

---
+++ cikmissoru5.py
@@ -0,0 +1,15 @@
+
+def is_Pandigital(nr,n):
+    digits = ''.join(map(str,range(1,n+1)))
+    nr = str(nr)
+    for i in digits:
+        if i not in nr[0:n]:
+            return False
+    return True
+
+toplam = 0
+n = input("n degerini giriniz : ")
+for i in range(100,1000):
+    if is_Pandigital(i,n):
+        toplam += i
+print toplam
